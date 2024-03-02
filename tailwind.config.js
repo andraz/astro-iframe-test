841f53a937457ccf4064d842264cfbe96c17e2e7
Andraž Kos <andraz.kos@gmail.com>
2024-03-02 12:45:50 +0000

    Add tailwindcss support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.astro'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
